ab936105168c228425addc62365424308ef71f7d
barouki <ayman.laaroussi.elbarouki@gmail.com>
2024-03-29 03:00:26 +0000

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  console.log("l9lawi")
+  const now = new Date()
+  const a = 10
+  const b = 100
+  console.log(a+b)
+  console.log(now)
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
